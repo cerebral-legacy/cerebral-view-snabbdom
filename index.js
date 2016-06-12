6990c95afbe40a5e34c6acd2dca9f50e52f76b82
Christian Alfoni <christianalfoni@gmail.com>
2016-06-12 14:17:23 +0200

    Support new state tree and debugger

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+/* global CustomEvent */
 var snabbdom = require('snabbdom')
 var h = require('snabbdom/h')
 var html
 try { html = require('snabbdom-jsx').html } catch (e) {}
 
-var optimized = {}
+var componentsMap = {}
 var activeController = null
+
+function extractDeps (deps, allDeps) {
+  return Object.keys(deps).reduce(function (depsMap, key) {
+    if (deps[key].getDepsMap) {
+      return extractDeps(deps[key].getDepsMap(), allDeps)
+    } else {
+      var depsKey = Array.isArray(deps[key]) ? deps[key].join('.') : deps[key]
+      depsMap[depsKey] = true
+    }
+    return depsMap
+  }, allDeps)
+}
 
 var patch = snabbdom.init([
   require('snabbdom/modules/class'),
@@ -12,86 +25,157 @@ var patch = snabbdom.init([
   require('snabbdom/modules/attributes'),
   require('snabbdom/modules/style'),
   require('snabbdom/modules/eventlisteners'),
-  {
-    destroy: function (vnode) {
-      if (vnode.key) {
-        delete optimized[vnode.key]
+  process.env.NODE_ENV === 'production' ? {} : {
+    create: function (emptyVnode, vnode) {
+      if (vnode.component) {
+        var deps = vnode.component.getStatePaths(vnode.component.props, vnode.component.deps)
+        registerComponent(vnode, extractDeps(deps, {}))
       }
+    },
+    update: function (prevNode, newNode) {
+      if (prevNode.component && newNode.component) {
+        var props = newNode.component.props
+        var getStatePaths = newNode.component.getStatePaths
+        var deps = newNode.component.deps
+        newNode.component = prevNode.component
+        newNode.component.props = props
+        newNode.component.getStatePaths = getStatePaths
+        newNode.component.deps = deps
+        deps = newNode.component.getStatePaths(newNode.component.props, newNode.component.deps)
+        updateComponent(newNode, extractDeps(deps, {}))
+      }
+    },
+    remove: function (vnode, removeCallback) {
+      if (vnode.component) {
+        unregisterComponent(vnode)
+      }
+      removeCallback()
     }
   }
 ])
 
-var hasChanged = function (oldProps, oldState, newProps, newState) {
-  if (
-    Object.keys(oldProps).length !== Object.keys(newProps).length ||
-    Object.keys(oldState).length !== Object.keys(newState).length
-  ) {
-    return true
-  }
-  var key
-  for (key in oldProps) {
-    if (oldProps[key] !== newProps[key]) {
-      return true
+function updateComponent (vnode, deps) {
+  unregisterComponent(vnode)
+  registerComponent(vnode, deps)
+}
+
+function registerComponent (vnode, deps) {
+  componentsMap = Object.keys(deps).reduce(function (componentsMap, key) {
+    componentsMap[key] = componentsMap[key] ? componentsMap[key].concat(vnode.component) : [vnode.component]
+    return componentsMap
+  }, componentsMap)
+}
+
+function unregisterComponent (vnode) {
+  Object.keys(componentsMap).forEach(function (key) {
+    if (componentsMap[key].indexOf(vnode.component) >= 0) {
+      componentsMap[key].splice(componentsMap[key].indexOf(vnode.component), 1)
     }
-  }
-  for (key in oldState) {
-    if (oldState[key] !== newState[key]) {
-      return true
+    if (componentsMap[key].length === 0) {
+      delete componentsMap[key]
     }
+  })
+}
+
+function getStatePaths (props, deps) {
+  if (!deps) {
+    return {}
   }
-  return false
+  var propsWithModules = Object.keys(props).reduce(function (propsWithModules, key) {
+    propsWithModules[key] = props[key]
+    return propsWithModules
+  }, {modules: activeController.getModules()})
+  return typeof deps === 'function' ? deps(propsWithModules) : deps
+}
+
+function getProps (props, deps, children) {
+  var paths = getStatePaths(props, deps)
+
+  var propsToPass = Object.keys(paths || {}).reduce(function (props, key) {
+    props[key] = paths[key].getDepsMap ? paths[key].get(activeController.get()) : activeController.get(paths[key])
+    return props
+  }, {})
+
+  propsToPass = Object.keys(props).reduce(function (propsToPass, key) {
+    propsToPass[key] = props[key]
+    return propsToPass
+  }, propsToPass)
+
+  propsToPass.signals = activeController.getSignals()
+  propsToPass.modules = activeController.getModules()
+
+  return propsToPass
+}
+
+function onCerebralUpdate (changes, runPatching, force) {
+  function traverse (level, currentPath, componentsToRender) {
+    Object.keys(level).forEach(function (key) {
+      currentPath.push(key)
+      var stringPath = currentPath.join('.')
+      if (componentsMap[stringPath]) {
+        componentsToRender = componentsMap[stringPath].reduce(function (componentsToRender, component) {
+          if (componentsToRender.indexOf(component) === -1) {
+            return componentsToRender.concat(component)
+          }
+          return componentsToRender
+        }, componentsToRender)
+      }
+      if (level[key] !== true) {
+        componentsToRender = traverse(level[key], currentPath, componentsToRender)
+      }
+      currentPath.pop()
+    })
+    return componentsToRender
+  }
+  var start = Date.now()
+  var componentsToRender = traverse(changes, [], [])
+  runPatching()
+  var end = Date.now()
+
+  if (process.env.NODE_ENV !== 'production' && (force || componentsToRender.length)) {
+    var devtoolsComponentsMap = Object.keys(componentsMap).reduce(function (devtoolsComponentsMap, key) {
+      devtoolsComponentsMap[key] = componentsMap[key].map(function (component) {
+        return component.name
+      })
+      return devtoolsComponentsMap
+    }, {})
+    var event = new CustomEvent('cerebral.dev.components', {
+      detail: {
+        map: devtoolsComponentsMap,
+        render: {
+          start: start,
+          duration: end - start,
+          changes: changes,
+          components: componentsToRender.map(function (component) {
+            return component.name
+          })
+        }
+      }
+    })
+    window.dispatchEvent(event)
+  }
 }
 
 function Component () {
-  var extractsState = arguments.length === 2
-  var statePaths = arguments[0]
-  var render = extractsState ? arguments[1] : arguments[0]
+  var hasName = typeof arguments[0] === 'string'
+  var name = hasName ? arguments[0] : 'NoName'
+  var extractsState = (hasName && arguments.length === 3) || arguments.length === 2
+  var deps = extractsState ? hasName ? arguments[1] : arguments[0] : {}
+  var getVNode = extractsState ? hasName ? arguments[2] : arguments[1] : arguments[0]
 
-  return function (props, children) {
-    var newState
-    if (extractsState) {
-      newState = Object.keys(statePaths).reduce(function (state, key) {
-        state[key] = activeController.get(statePaths[key])
-        return state
-      }, {})
-    } else {
-      newState = activeController.get()
-    }
-
-    if (
-      props &&
-      props.key &&
-      optimized[props.key] &&
-      !hasChanged(
-        optimized[props.key].props,
-        optimized[props.key].state,
-        props,
-        newState
-      )
-    ) {
-      return optimized[props.key].vnode
-    }
-
-    var vnode = render({
+  var render = function (props) {
+    var vnode = getVNode(getProps(props || {}, deps))
+    vnode.component = {
+      getStatePaths: getStatePaths,
       props: props || {},
-      children: children,
-      state: newState,
-      signals: activeController.getSignals(),
-      modules: activeController.getModules()
-    })
-
-    if (props && props.key) {
-      optimized[props.key] = {
-        vnode: vnode,
-        props: props,
-        state: newState
-      }
-      vnode.optimize = props.optimize
+      deps: deps,
+      name: name
     }
-
     return vnode
   }
-};
+
+  return render
+}
 
 Component.DOM = html
 
@@ -101,11 +185,12 @@ module.exports.h = h
 
 module.exports.render = function render (cb, el, controller) {
   activeController = controller
-  var prevNode = cb()
-  controller.on('change', function () {
-    var newNode = cb()
-    patch(prevNode, newNode)
-    prevNode = newNode
+  var oldNode = null
+  activeController.on('flush', function (changes) {
+    onCerebralUpdate(changes, function () {
+      oldNode = patch(oldNode, h('div', [cb()]))
+    })
   })
-  patch(el, prevNode)
+  oldNode = patch(el, h('div', [cb()]))
+  onCerebralUpdate({}, function () {}, true)
 }
